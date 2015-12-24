f4d46ad48eb7c7327454d0767e7e90b596c9c363
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2015-12-23 20:28:33 -0500

    use I for Integer param

--- src/arith/mul.jl
+++ src/arith/mul.jl
@@ -7,7 +7,7 @@
 =#
 
 function (*){G<:Grasp,W<:Grasp,R<:Real}(a::Rvl{G,R}, b::Rvl{W,R})
-    z = zero(F)
+    z = zero(R)
     if     a.hi <= z
        if     b.hi <= z
            mulLteLte(a,b)
@@ -62,7 +62,7 @@ end
 (*){G<:Grasp,R<:Real}(a::Rvl{G,R}, b::R) = (*)(a, Rvl{G,R}(b))
 (*){G<:Grasp,R<:Real}(a::R, b::Rvl{G,R}) = (*)(Rvl{G,R}(a), b)
 
-(*){G<:Grasp,F<:WorkInt}(a::Rvl{G,Float64}, b::F) = (*)(a, Rvl{G,Float64}(convert(Float64,b)))
-(*){G<:Grasp,F<:WorkInt}(a::F, b::Rvl{G,Float64}) = (*)(Rvl{G,Float64}(convert(Float64,a)), b)
-(*){G<:Grasp,F<:WorkInt}(a::Rvl{G,Float32}, b::F) = (*)(a, Rvl{G,Float32}(convert(Float32,b)))
-(*){G<:Grasp,F<:WorkInt}(a::F, b::Rvl{G,Float32}) = (*)(Rvl{G,Float32}(convert(Float32,a)), b)
+(*){G<:Grasp,I<:WorkInt}(a::Rvl{G,Float64}, b::I) = (*)(a, Rvl{G,Float64}(convert(Float64,b)))
+(*){G<:Grasp,I<:WorkInt}(a::I, b::Rvl{G,Float64}) = (*)(Rvl{G,Float64}(convert(Float64,a)), b)
+(*){G<:Grasp,I<:WorkInt}(a::Rvl{G,Float32}, b::I) = (*)(a, Rvl{G,Float32}(convert(Float32,b)))
+(*){G<:Grasp,I<:WorkInt}(a::I, b::Rvl{G,Float32}) = (*)(Rvl{G,Float32}(convert(Float32,a)), b)
--- src/arith/sub.jl
+++ src/arith/sub.jl
@@ -18,7 +18,7 @@ end
 (-){G<:Grasp,R<:Real}(a::Rvl{G,R}, b::R) = (-)(a, Rvl{G,R}(b))
 (-){G<:Grasp,R<:Real}(a::R, b::Rvl{G,R}) = (-)(Rvl{G,R}(a), b)
 
-(-){G<:Grasp,F<:WorkInt}(a::Rvl{G,Float64}, b::F) = (-)(a, Rvl{G,Float64}(convert(Float64,b)))
-(-){G<:Grasp,F<:WorkInt}(a::F, b::Rvl{G,Float64}) = (-)(Rvl{G,Float64}(convert(Float64,a)), b)
-(-){G<:Grasp,F<:WorkInt}(a::Rvl{G,Float32}, b::F) = (-)(a, Rvl{G,Float32}(convert(Float32,b)))
-(-){G<:Grasp,F<:WorkInt}(a::F, b::Rvl{G,Float32}) = (-)(Rvl{G,Float32}(convert(Float32,a)), b)
+(-){G<:Grasp,I<:WorkInt}(a::Rvl{G,Float64}, b::I) = (-)(a, Rvl{G,Float64}(convert(Float64,b)))
+(-){G<:Grasp,I<:WorkInt}(a::I, b::Rvl{G,Float64}) = (-)(Rvl{G,Float64}(convert(Float64,a)), b)
+(-){G<:Grasp,I<:WorkInt}(a::Rvl{G,Float32}, b::I) = (-)(a, Rvl{G,Float32}(convert(Float32,b)))
+(-){G<:Grasp,I<:WorkInt}(a::I, b::Rvl{G,Float32}) = (-)(Rvl{G,Float32}(convert(Float32,a)), b)
