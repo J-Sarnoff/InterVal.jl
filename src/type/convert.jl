a5725869daaebd9892a14cb00ede932350a1a8a7
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2015-12-24 00:11:10 -0500

    interconvert ValidatedNumerics

--- src/type/convert.jl
+++ src/type/convert.jl
@@ -1,4 +1,47 @@
+#=
 for (fn) in (:ClCl, :ClOp, :OpCl, :OpOp)
     @eval ($fn){R1<:Real, R2<:Real}(a::R1, b::R2) = ($fn)(promote(a,b)...)
+end
+=#
+
+# grasp
+# it takes values presumed exact 
+# adjusting as needed for (from closedclosed into Grasp)
+# so there is no risk of value exclusion 
+# or
+# it takes values as grasped
+# and adjusts as needed (from Grasp into closedclosed)
+# so there is no risk of value exclusion 
+#
+# grasped  yields a grasp_ed interval
+# released yields a twice grasp_ed interval 
+
+grasp{R<:Real}(::Type{ClCl}, a::R, b::R) = (a,b)
+grasp{R<:Real}(::Type{ClOp}, a::R, b::R) = (a,nextfloat(b))
+grasp{R<:Real}(::Type{OpCl}, a::R, b::R) = (prevfloat(a),b)
+grasp{R<:Real}(::Type{OpOp}, a::R, b::R) = widen(a,b)
+
+grasped{G<:Grasp, R<:Real}(::Type{G}, a::R, b::R) = Rvl{G,R}( grasp(::Type{G},a,b)... )
+release{G<:Grasp, R<:Real}(::Type{G}, a::R, b::R) = grasp(::Type{G}, a, b)
+release{G<:Grasp, R<:Real}(this::Rvl{G,R})  = release(::Type{G}, values(this)... )
+released{G<:Grasp, R<:Real}(this::Rvl{G,R}) = Rvl{G,R}( release(::Type{G},a,b)... )
+
+
+
+if ::isdefined(Main,:ValidatedNumerics)
+
+  import ValidatedNumerics.Interval, ValidatedNumerics.@interval
+   
+  convert{R<:Real}(::Type{Rvl{ClCl}}, x::Interval{R}) = ClCl(x.lo,x.hi)
+  convert{R<:Real}(::Type{Rvl}, x::Interval{R}) = convert(Rvl{ClCl}, x)
+  function  convert{G<:Grasp,R<:Real}(::Type{Rvl{G,R}, x::Interval{R})
+      lo, hi = grasp(G, x.lo, x.hi)
+      Rvl{G,R}(lo, hi)
+  end
+  function  convert{G<:Grasp,R<:Real}(::Type{Interval{R}}, x::Rvl{G,R})
+      lo, hi = release(G, x.lo, x.hi)
+      @interval(lo, hi)
+  end
+  
 end
 
--- src/type/interval.jl
+++ src/type/interval.jl
@@ -31,21 +31,28 @@ upperbound{G<:Grasp, R<:Real}(x::Rvl{G,R}) = x.hi
 
 #=
 aClCl = Rvl{ClCl,Float64}(1.0,2.0); aClCl == ClCl(1.0,2.0)
-aOpCl = Rvl{OpCl,Float64}(1.0,2.0); aOpCl == ClCl(1.0,2.0)
-aClOp = Rvl{ClOp,Float64}(1.0,2.0); aClOp == ClCl(1.0,2.0)
-aOpOp = Rvl{OpOp,Float64}(1.0,2.0); aOpOp == ClCl(1.0,2.0)
+aOpCl = Rvl{OpCl,Float64}(1.0,2.0); aOpCl == OpCl(1.0,2.0)
+aClOp = Rvl{ClOp,Float64}(1.0,2.0); aClOp == ClOp(1.0,2.0)
+aOpOp = Rvl{OpOp,Float64}(1.0,2.0); aOpOp == OpOp(1.0,2.0)
 =#
 
 for B in (:Self, :ClCl, :ClOp, :OpCl, :OpOp)
   @eval begin
+     function convert{R<:Real}(::Type{Rvl{($B),R}}, lo::R, hi::R)
+        lo, hi = minmax(lo, hi)
+        Rvl{$B,R}(lo,hi)
+     end
+     convert{R<:Real}(::Type{Rvl{($B),R}}, x::R) = convert(Rvl($B,R), x,x)
      function ($B){R<:Real}(lo::R, hi::R)
         lo, hi = minmax(lo, hi)
         Rvl{$B,R}(lo,hi)
+     end
+     function ($B){R1<:Real,R2<:Real}(lo::R1, hi::R2)
+        ($B)(promote(lo,hi)...)
      end
      function ($B){R<:Real}(x::R)
         Rvl{$B,R}(x,x)
      end
   end
 end
-
 
