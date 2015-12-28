0d049771e51a3b66e5b85940b47dfa119d3f7b04
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2015-12-28 01:40:55 -0500

    sqrt

--- src/arith/sqrt.jl
+++ src/arith/sqrt.jl
@@ -1,9 +1,6 @@
-for T in (:ClCl, :ClOp, :OpCl, :OpOp)
-   @eval begin
-       function (sqrt){R<:Real}(a::Rvl{$T,R})
-           lo = (sqrt)(a.lo, RoundDown)
-           hi = (sqrt)(a.hi, RoundUp)
-           Rvl{$T,R}(lo,hi)
-       end
-   end       
+function (sqrt){G<:Grasp,R<:Real}(a::Rvl{G,R})
+    lo = (sqrt)(a.lo, RoundDown)
+    hi = (sqrt)(a.hi, RoundUp)
+    Rvl{G,R}(lo, hi)
 end
+
