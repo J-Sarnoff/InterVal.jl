a4648d540ac5e64194da4ebab690136d6591a9e3
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2015-12-28 00:59:27 -0500

    inf already assigned

--- src/number/givenvals.jl
+++ src/number/givenvals.jl
@@ -3,7 +3,7 @@
 zero{G<:Grasp, R<:Real}(::Type{G},::Type{R}) = (G)(zero(R), zero(R))
  one{G<:Grasp, R<:Real}(::Type{G},::Type{R}) = (G)( one(R),  one(R))
 
- Inf{G<:Grasp, R<:Real}(::Type{G},::Type{R}) = (G)((R)(Inf), (R)(Inf))
- NaN{G<:Grasp, R<:Real}(::Type{G},::Type{R}) = (G)((R)(NaN), (R)(NaN))
+# Inf{G<:Grasp, R<:Real}(::Type{G},::Type{R}) = (G)((R)(Inf), (R)(Inf))
+# NaN{G<:Grasp, R<:Real}(::Type{G},::Type{R}) = (G)((R)(NaN), (R)(NaN))
 
 
